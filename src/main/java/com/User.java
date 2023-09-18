28698da89ad543e5a4374408a74c2c6d1929b270
MalkitKaur <malkit27407@gmail.com>
2023-09-18 10:59:15 -0400

    sept 18-added validation for the username

--- src/main/java/com/User.java
+++ src/main/java/com/User.java
@@ -11,4 +11,35 @@ public class User{
         setUserName (userName);
         setPhone (phone);
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    /**
+     * This sets the users name, it cannot be blank
+     * @param userName - whatever the user wants it to be
+     * */
+    public void setUserName(String userName) {
+        userName=userName.trim();//remove leading and trailing white spaces
+        if(userName.matches("[A-z1-9]+"))
+            this.userName = userName;
+        else
+            throw new IlleagalArgumentException("username must have at least 1 character and/or number");
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
